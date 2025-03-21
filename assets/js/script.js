aa044c5f768c00467f865c68cf4dd77db1c7c165
Phu Vo <u1153426@umail.usq.edu.au>
2025-03-22 07:54:42 +1000

    ✨ Implement Add New Schedule - Section 8 - Create Tour page

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,22 @@ if (buttonMenuMobile) {
   })
 }
 // End Show/Hide Menu Mobile
+
+// Add New Schedule - Section 8 - Create Tour page
+const scheduleSection8 = document.querySelector('.section-8 .inner-schedule')
+if (scheduleSection8) {
+  const buttonCreate = scheduleSection8.querySelector('.inner-schedule-create')
+  const listItem = scheduleSection8.querySelector('.inner-schedule-list')
+
+  // Add New Schedule
+  buttonCreate.addEventListener('click', () => {
+    const firstItem = listItem.querySelector('.inner-schedule-item')
+    const cloneItem = firstItem.cloneNode(true)
+
+    cloneItem.querySelector('.inner-schedule-head input').value = ''
+    cloneItem.querySelector('.inner-schedule-body textarea').value = ''
+
+    listItem.appendChild(cloneItem)
+  })
+}
+// End Add New Schedule - Section 8 - Create Tour page
