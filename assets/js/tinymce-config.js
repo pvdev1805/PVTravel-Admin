3533ffb3aaa6ed3ee8f1d608495f544bc63d593b
Phu Vo <u1153426@umail.usq.edu.au>
2025-03-25 15:26:04 +1000

    ✨ Implement TinyMCE integration as Text Editor - Create Category page

---
+++ assets/js/tinymce-config.js
@@ -0,0 +1,6 @@
+tinymce.init({
+  selector: '[textarea-mce]',
+  plugins: 'charmap codesample emoticons help image link lists advlist media preview searchreplace table wordcount',
+  toolbar:
+    'undo redo | styles | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | outdent indent | charmap | codesample | emoticons | image | link | numlist bullist | media | preview | searchreplace | help'
+})
